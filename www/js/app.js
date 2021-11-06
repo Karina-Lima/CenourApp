ff7b3b495dae2851cbaeed3a36f3ae7fb53e73d8
Karina Lima <karinaxyxx@gmail.com>
2021-11-06 19:55:19 -0300

    atualização app

---
+++ www/js/app.js
@@ -0,0 +1,34 @@
+$('.collection')
+    .on('click', '.collection-item', function() {
+        var nomeProduto = this.firstChild.textContent;
+        Materialize.toast(nomeProduto + ' adicionado', 1000);
+
+        var $badge = $('.badge', this);
+        if ($badge.length === 0) {
+            $badge = $('<span class="badge brown-text">0</span>').appendTo(this);
+        }
+
+        $badge.text(parseInt($badge.text()) + 1);
+    })
+    .on('click', '.badge', function() {
+        $(this).remove();
+        return false;
+    });
+
+$('.modal-trigger').leanModal();
+
+$('#confirmar').on('click', function() {
+    var texto = "";
+
+    $('.badge').parent().each(function() {
+        texto += this.firstChild.textContent + ': ';
+        texto += this.lastChild.textContent + ', ';
+    });
+
+    $('#resumo').empty().text(texto);
+});
+
+$('.acao-limpar').on('click', function() {
+    $('#numero-mesa').val('');
+    $('.badge').remove();
+});
